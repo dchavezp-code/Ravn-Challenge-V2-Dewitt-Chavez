8934f6081e8016626ccb27511bf950774c1d3042
Dewitt Scott Chavez Ponce <dchavezp@unsa.edu.pe>
2021-01-23 22:46:19 -0500

    Set up the project

---
+++ src/routes/main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './main.module.css'
+function Main() {
+    return (
+        //To do: 
+        <div className={styles.container}>
+            Hola
+        </div>
+    )
+}
+
+export default Main
